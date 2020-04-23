095c4ab7e1bbd315059aac9457abd2fb2bb09f0f
Ubuntu <ubuntu@ip-172-31-34-10.us-east-2.compute.internal>
2020-04-23 18:47:59 +0000

    homework 1 finished

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,60 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  # arr.sum
+  result = 0
+  arr.each {|x| result += x}
+  result
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  #result = 0
+  #if arr.length == 1
+  #  result =  arr[0]
+  #elsif arr.length == 1
+  #  arr.sort!
+  #  result =  arr[-1] + arr[-2]
+  # sum(arr.sort.last(2))
+  sum(arr.max(2))
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2) { |c| return true if c.sum == n}
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[bcdfghjklmnprstvwxyz]/i.match(s)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  #if /^[01]+$/.match?(s)
+  #else
+  #  false
+  #end
+/^(0|00|([01]+00))$/.match?(s)
 end
+
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize isbn, price
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format("$%.2f", @price)
+  end
 end
+
